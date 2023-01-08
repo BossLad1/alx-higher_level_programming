0a62f683518ba2cb6e5eac70753cfb47980271a7
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-01-08 02:11:11 -0800

    python task 1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for j in range(0,100):
-    if j != 99:
-        print("{:02d}, ".format(j), end="")
-    else:
-        print("{:02}".format(j))
+for j in range(100):
+    print(f"{j:02d}", end=", " if j < 99 else "\n")
