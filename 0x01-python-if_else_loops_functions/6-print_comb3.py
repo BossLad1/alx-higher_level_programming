364a8e4d05662721c58cc751bfd1e916231a1103
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-01-08 02:07:07 -0800

    python task 1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(1, 10):
-        if j >= j:
-            continue
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end="")
+for i in range(0):
+    for j in range(10):
+        if i != j:
+            print("{}{}".format(i, j), end=" ")
+print()
