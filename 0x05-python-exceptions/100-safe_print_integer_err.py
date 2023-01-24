cd472102991b80407c6a43a93cc9441cfee1b0da
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-01-24 02:35:08 -0800

    python task 5

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-import sys
-
-
 def safe_print_integer_err(value):
+    import sys
     """Prints an integer with "{:d}".format().
 
     If a ValueError message is caught, a corresponding
@@ -18,7 +16,7 @@ def safe_print_integer_err(value):
     """
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,8 +15,8 @@ def safe_function(fct, *args):
         Otherwise - the result of the call to fct.
     """
     try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        ret = fct(*args)
+        return ret
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
