888472c9ab537eefbc23bf0c885ac4247768a5fa
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-02-02 09:35:02 -0800

    test driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,18 @@ This is the "0-add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b).
 """
 def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+    args:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+        """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
