8927d99ba141a08e3968602bd93351862736b268
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-02-08 08:11:09 -0800

    input output tasks

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
 """Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    structure = {}
+    if hasattr(obj, "__dict__"):
+        structure = obj.__dict__.copy()
+    return structure
