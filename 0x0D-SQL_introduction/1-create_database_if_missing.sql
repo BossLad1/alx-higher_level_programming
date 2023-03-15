32e04219d78079fb1d3d723e11c393c7ebaf16b3
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-03-14 17:15:55 -0700

    SQL introduction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
---- creates databas htbn_0c_0 in your MySQL server.
+-- creates databas htbn_0c_0 in your MySQL server.
+
 CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
---- updates the score of Bob to 10 in the table second_table.
---- You are not allowed to use Bob’s id value, only the name field
+-- updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
 
 UPDATE `second_table` 
 SET `score` = 10 
