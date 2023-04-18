96be5e7407e34ae41f0b017e8d90f7d443ccb3a6
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-04-18 16:11:35 -0700

    Javascript warmup task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-
-
+// a script that prints a message depending of the number of argument passed.
+const i = process.argv.length;
+if (i === 3 && i > 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
