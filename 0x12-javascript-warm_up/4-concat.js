eb391d79545fa7596392eba6997e15a8787a61e6
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-04-18 16:45:25 -0700

    Javascript warmup projects

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 // a script that prints two arguments passed to it, in the following format: 'is'
 const i = process.argv[2];
 const j = process.argv[3];
-console.log(i + 'is' + j);
+console.log(i + ' is ' + j);
