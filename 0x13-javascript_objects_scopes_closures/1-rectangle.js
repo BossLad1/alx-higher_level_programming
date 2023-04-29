2d1ecf78050ecd48be0bbcdb61a3735f59b50d10
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-04-29 09:20:51 -0700

    Javascript Objects Scopes Closures

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+class Rectangle {
+  constructor(width, height) {
+    this.width = w;
+    this.height = h; }
+}
+module.exports = Rectangle;
