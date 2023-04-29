e11aba85a3cc37794be852cdcd88441dd0910aa3
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-04-29 10:04:15 -0700

    Javascript Objects Scopes Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,9 +9,11 @@ class Rectangle {
       this.width = w;
       this.height = h;
     }
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
   }
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
 }
 module.exports = Rectangle;
