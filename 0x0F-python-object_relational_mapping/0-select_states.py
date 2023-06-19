d0e36f8f9f7dd791cbc1761742dcb61abb8027ad
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-06-19 04:15:39 -0700

    python ORM project

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,13 +8,13 @@ from sys import argv
 
 if __name__ == '__main__':
     """create a connection to the database"""
-    db_connect = MySQLdb.connect(host="locatlhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host="locatlhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3])
 
     """gives ability to have multiple separate working environment"""
-    db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
 
-    rows_selected = db_cursor.fetchall()
-    for i in rows_selected:
+    rows = cur.fetchall()
+    for i in rows:
         print(i)
