5bb9781ed62e7ecfc256c0eba37290eb5f329e37
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-06-19 04:02:39 -0700

    python ORM project

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """create a connection to the database"""
+    db = MYSQL.connect(host="locatlhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3])
+
+    """gives ability to have multiple separate working environment"""
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states")
+
+    rows = db_cursor.fetchall()
+    for i in rows:
+        print(i)
+
+    """clean and close process"""
+    db_cursor.close()
