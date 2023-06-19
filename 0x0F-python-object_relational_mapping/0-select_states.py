8813bf9bd7dd83872f775b9e7038f54f7761af4c
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-06-19 04:10:28 -0700

    python ORM project

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
+""" script that lists all states from 
+the database hbtn_0e_0_usa """
 
 
 import MySQLdb
@@ -7,15 +8,15 @@ from sys import argv
 
 if __name__ == '__main__':
     """create a connection to the database"""
-    db = MYSQL.connect(host="locatlhost", port=3306, user=argv[1],
+    db_connect = MySQLdb.connect(host="locatlhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3])
 
     """gives ability to have multiple separate working environment"""
     db_cursor = db_connect.cursor()
     db_cursor.execute("SELECT * FROM states")
 
-    rows = db_cursor.fetchall()
-    for i in rows:
+    rows_selected = db_cursor.fetchall()
+    for i in rows_selected:
         print(i)
 
     """clean and close process"""
