a0afae7c501f6d4565c08e8d9a51b26c936139fe
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-06-19 15:27:26 -0700

    ORM task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
-
-
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """create a connection to the database"""
-    db = MySQLdb.connect(host="locatlhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-    """gives ability to have multiple separate working environment"""
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
 
+def list_states(username, password, database):
+    """a function to list states"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
