7389c2200cf3b3e3bc65007ceb3b94680abc8d12
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-08-06 02:14:57 -0700

    Javascript web-jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+/* updates the text color of the <header> element to red (#FF0000) */
+const textColor = document.querySelector('header');
+textColor.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+textColor = $('#red_header');
+textColor.css('color', '#FF0000');
