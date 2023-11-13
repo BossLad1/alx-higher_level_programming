0107d13fd668cae7b6689ad100ccaf8d6701ae8e
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-13 02:58:41 -0800

    Javascript warmup project

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//a script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+
+const mulTi = ['C is fun', 'Python is cool', 'JavScript is amazing'];
+let i = 0;
+
+while (i < mulTi.length) {
+   console.log(mulTi[i]);
+   i++;
+}
+
