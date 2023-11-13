1d0ead6fa0763d15cc4f5a5144d7cade08eed92f
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-13 09:08:56 -0800

    JavaScript warmup project

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // a script that prints 3 line
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
