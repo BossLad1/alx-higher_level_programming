af7ec9b330ff92a8e614bd44223e23f4d37f8597
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-14 07:33:44 -0800

    JavaScript Project

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,13 +20,12 @@ class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    this.width *= 2;
-    this.height *= 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 
