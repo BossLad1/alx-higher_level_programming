c7721982b1d22cb85ae6a9b800c4aedabaa5a17e
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-14 06:23:47 -0800

    JavaScripts Project

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// empty class Rectangle that defines a rectangle:
+
+class Rectangle {
+  constructor(w, h) {
+    this.w = width;
+    this.h = height;
+  }
+}
+
+module.exports = Rectangle;
