bcd549d1e59cf3d4848a883f271d173aad5e22f8
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-14 08:10:58 -0800

    JavaScript Project

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  return list.map((_, i) => list[list.length - 1 - i]);
+};
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// the number of arguments already printed and the new argument value
+let argc = 0;
+
+exports.logMe = function (item) {
+  console.log(`${argc}: ${item}`);
+  argc++;
