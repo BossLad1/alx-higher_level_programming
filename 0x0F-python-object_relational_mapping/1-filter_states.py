a9a95009d525b6c7df825fa75e4d642ee207dbe1
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-11-18 04:52:23 -0800

    python ORM  project

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,13 @@ database `hbtn_0e_0_usa`.
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
+"""
     Access to the database and get the states
     from the database.
     """
+
+if __name__ == '__main__':
+
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
